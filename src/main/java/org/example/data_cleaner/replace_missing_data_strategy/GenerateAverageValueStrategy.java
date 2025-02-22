8b17a645b1ce93d348cf0d2d876e1668f77b069b
carolCheng <chengle0575@foxmail.com>
2025-02-22 17:01:39 +0800

    Fix bugs when generate value with algo: ignore empty value

--- src/main/java/org/example/data_cleaner/replace_missing_data_strategy/GenerateAverageValueStrategy.java
+++ src/main/java/org/example/data_cleaner/replace_missing_data_strategy/GenerateAverageValueStrategy.java
@@ -9,7 +9,8 @@ public class GenerateAverageValueStrategy implements GenerateValueForMissingData
      * Generates the average value for a specified key across a list of data maps.
      *
      *  This method assumes that the value associated with the specified key in each map
-     *  is a numeric type (such as Integer, Double, Long, etc.)
+     *  store the numeric number as string.
+     *
      *  @param rawData The list of maps containing the data to be processed.
      *  @param key The key whose values will be averaged.
      *  @return The calculated average as a Double.
@@ -19,15 +20,20 @@ public class GenerateAverageValueStrategy implements GenerateValueForMissingData
         double sum=0;
         int size= rawData.size();
         for(Map<String,Object> data:rawData){
-            Object value=data.getOrDefault(key,null);
+            Object value=data.getOrDefault(key,0);
 
-            if(value==null){
-                sum+=0;
-            }else if(value instanceof Number){
-                sum+=((Number) value).doubleValue();
-            }else{
-                throw new IllegalArgumentException("The raw data has some has invalid value for the key");
+            try{
+                if (value instanceof String stringValue) {
+                    if(value=="") continue; //ignore the empty value when counting the sum
+                    sum +=  Double.valueOf(stringValue);
+                } else {
+                    // If value is already a number, directly add it
+                    sum += ((Number) value).doubleValue();
+                }
+            } catch (RuntimeException e) {
+                throw new RuntimeException("Some value in the data for this key: "+ key+" cannot be casted to number");
             }
+
         }
         return sum/size;
     }
